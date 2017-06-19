f4c0a6b962eaad82ef804723095e10bd3b066b24
Rinzen Necroforger <Necroforger@gmail.com>
2017-06-19 16:45:49 -0400

    Textify command line program with ability to play gifs

--- cmd/textify/main.go
+++ cmd/textify/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"flag"
+	"fmt"
 	"image"
+	"image/gif"
 	"io"
 	"log"
+	"net/http"
 	"os"
 	"strings"
 	"time"
@@ -20,6 +24,9 @@ var (
 	Thumbnail = flag.Bool("t", false, "Resizes the image as a thumbnail")
 	StrideW   = flag.Float64("sw", 1, "Stride width parameter")
 	StrideH   = flag.Float64("sh", 2, "Stride height parameter")
+
+	IsGif   = flag.Bool("g", false, "Encode the image as a gif")
+	PlayGif = flag.Bool("pg", false, "Play the supplied gif image")
 
 	CropLeft   = flag.Uint("cl", 0, "Crop left parameter")
 	CropRight  = flag.Uint("cr", 0, "Crop right parameter")
@@ -59,17 +66,45 @@ func main() {
 		return
 	}
 
-	Source, err = os.Open(flag.Arg(0))
-	if err != nil {
-		log.Println(err)
-		return
+	path := flag.Arg(0)
+	if !strings.HasPrefix(path, "http://") && !strings.HasPrefix(path, "https://") {
+		f, err := os.Open(flag.Arg(0))
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		defer f.Close()
+		Source = f
+	} else {
+		resp, err := http.Get(path)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		defer resp.Body.Close()
+		Source = resp.Body
 	}
 
-	// Decode image
-	img, _, err := image.Decode(Source)
-	if err != nil {
-		log.Println(err)
-		return
+	var (
+		img    image.Image
+		gifimg *gif.GIF
+	)
+
+	// Decode as image
+	if !*IsGif && !*PlayGif {
+		img, _, err = image.Decode(Source)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		// Decode as gif
+	} else {
+		gifimg, err = gif.DecodeAll(Source)
+		if err != nil {
+			log.Println(err)
+			return
+		}
 	}
 
 	// Initialize destination stream
@@ -85,11 +120,57 @@ func main() {
 		Dest = os.Stdout
 	}
 
-	err = textify.NewEncoder(Dest).Encode(img, Options)
+	if *PlayGif {
+		type Frame struct {
+			Delay int
+			Text  string
+		}
+		frames := []Frame{}
+		decoder := textify.EncodeGif(gifimg, Options)
+		var (
+			frame string
+			err   error
+			delay int
+		)
+
+		lastWrite := time.Now()
+		for frame, delay, err = decoder.NextFrame(); err == nil; frame, delay, err = decoder.NextFrame() {
+			// Do not allow the gif to display too fast while rendering.
+			if d, u := (time.Millisecond * 10 * time.Duration(delay)), time.Now().Sub(lastWrite); u < d {
+				time.Sleep(d - u)
+			}
+			frames = append(frames, Frame{Text: frame, Delay: delay})
+			fmt.Fprintln(Dest, frame)
+			lastWrite = time.Now()
+		}
+		for {
+			for _, f := range frames {
+				fmt.Fprintln(Dest, f.Text)
+				time.Sleep(time.Millisecond * 10 * time.Duration(f.Delay))
+			}
+		}
+	}
+
+	// Using a buffered writer helps to display the image to the console smoother.
+	bufwriter := bufio.NewWriterSize(Dest, 2048)
+
+	// Encode gif
+	if *IsGif {
+		err = textify.NewGifEncoder(bufwriter).Encode(gifimg, Options)
+		// Encode image
+	} else {
+		err = textify.NewEncoder(bufwriter).Encode(img, Options)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+	}
+
+	// Flush the writer to append any data still remaining in the buffer
+	err = bufwriter.Flush()
 	if err != nil {
 		log.Println(err)
 		return
 	}
-	time.Sleep(time.Millisecond * 300)
 
 }
